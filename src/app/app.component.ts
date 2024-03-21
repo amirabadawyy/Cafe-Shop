60b468ef1ba8b8c166f9f56c2989d8a5428862c5
amira <162512223+amira-badawyy@users.noreply.github.com>
2024-03-22 01:15:50 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ShopComponent } from './Components/shop/shop.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, NavBarComponent,FooterComponent,RouterOutlet,RouterModule,HomeComponent,ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
