80f9338e75bb931665d361ddba2b8c6538bb32f0
Amira Badawy <amiraabouelezz2001@gmail.com>
2024-04-07 01:29:58 +0200

    Update user.service.ts

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
   constructor(private http:HttpClient) { }
-  private DB_URL =  "http://localhost:3000/users"
+  private DB_URL =  "https://api.myjson.online/v1/records/f79434d2-e380-42b0-ba25-8613c072166a"
   getAllUsers(){
     return this.http.get(this.DB_URL);
   }
