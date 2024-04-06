4657fa4cf1935ff035f3a8995b0d4bcea3733253
Amira Badawy <amiraabouelezz2001@gmail.com>
2024-04-07 01:29:40 +0200

    Update reviews.service.ts

--- src/app/Services/reviews.service.ts
+++ src/app/Services/reviews.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ReviewsService {
-  private DB_URL='http://localhost:3000'
+  private DB_URL='https://api.myjson.online/v1/records/f79434d2-e380-42b0-ba25-8613c072166a'
   constructor(private http:HttpClient) { }
   GetReviews(){
     return this.http.get(`${this.DB_URL}/Reviews`)
