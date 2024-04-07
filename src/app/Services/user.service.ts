7c65deec45b0e65e97136e27509cfa4dfeeeb336
Amira Badawy <amiraabouelezz2001@gmail.com>
2024-04-07 04:33:31 +0200

    Update user.service.ts

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
   constructor(private http:HttpClient) { }
-  private DB_URL =  "https://981a49c17b9f4225a6b3d4955bb5545b.api.mockbin.io/"
+  private DB_URL =  "https://cafeshop-data.onrender.com"
   getAllUsers(){
     return this.http.get(this.DB_URL);
   }
